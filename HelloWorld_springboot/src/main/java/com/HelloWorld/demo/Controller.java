bb1f961a6e4d2dcb43bc5165ef582d4130fd9a60
ValtteriT <valtteri.toivonen@valuemotive.com>
2022-02-15 15:27:22 +0200

    Try number 1.

---
+++ HelloWorld_springboot/src/main/java/com/HelloWorld/demo/Controller.java
@@ -0,0 +1,16 @@
+package com.HelloWorld.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	
+	@GetMapping
+	public String hello() {
+		return "hello World!";
+
+	}
+
+}
---
+++ HelloWorld_springboot/src/test/java/com/HelloWorld/demo/HelloWorldSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HelloWorld.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
