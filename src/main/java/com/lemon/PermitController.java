5a31c4d1528adba194e8a9c2f12ac4289b3d1e3d
Mathias Hannus <ext-mathias.hannus@trafi.fi>
2022-02-28 16:18:27 +0200

    Added thymeleaf lib and template

--- src/main/java/com/lemon/PermitController.java
+++ src/main/java/com/lemon/PermitController.java
@@ -2,11 +2,14 @@ package com.lemon;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
 
 @Controller
 public class PermitController {
-	@GetMapping("/")
-	public String hello() {
+	@GetMapping("/hello")
+	public String hello(Model model) {
+		model.addAttribute("name", "test");
 		return "hello";
 		
 	}
