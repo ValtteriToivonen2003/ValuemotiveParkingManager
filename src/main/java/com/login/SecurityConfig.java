d56f5ba180f36f7244c6e29f5cda9dd0d49998b2
ValtteriT <valtteri.toivonen@valuemotive.com>
2022-03-15 12:11:20 +0200

    Google oauth2 error code "Error 400: redirect_uri_mismatch" output

--- src/main/java/com/login/SecurityConfig.java
+++ src/main/java/com/login/SecurityConfig.java
@@ -2,20 +2,31 @@ package com.login;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableOAuth2Sso
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		http
-			.csrf().disable()
-			.antMatcher("/**").authorizeRequests()
-			.antMatchers("/", "/homeNotSignedIn.html")
-			.permitAll().anyRequest()
-			.authenticated();
+			.authorizeRequests()
+				.antMatchers("/resources/**").permitAll()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/LoginPage.html")
+				.permitAll()
+				.and()
+			.oauth2Login()
+				.and()
+			.logout()
+				.permitAll();
+				
+
+			
 
 	}
 
---
+++ src/main/java/com/login/WebConfig.java
@@ -0,0 +1,18 @@
+package com.login;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+
+@EnableWebMvc
+@ComponentScan("org.springFramework.security.samples.mvc")
+public class WebConfig {
+	
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/loginPage").setViewName("login");
+		registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		
+	}
+
+}
